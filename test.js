4ac01008e32f55ffd813edc748717a27f79133b8
J Irving <j@lollyshouse.ca>
2014-01-07 15:10:57 -0500

    Updates - this code succeeded 2 times!

--- test.js
+++ test.js
@@ -5,15 +5,34 @@ var travel = require('./lib/travel'),
     arDrone = require('ar-drone'),
     client = arDrone.createClient();
 
+
+process.on('exit', function () {
+  stop();
+  process.exit(0);
+});
+
 client.takeoff();
 
 qrscan(client, function (error, code) {
   travel(client, code, function (error, client, callback) {
     if (error) {
       console.error(error);
+      stop();
       process.exit(1);
     }
 
+    stop();
     console.log("FTW!");
   });
 });
+
+function stop() {
+  client.stop();
+  client.land();
+}
+
+
+// travel(client, JSON.stringify({x:1,y:1}), function (error, client, callback) {
+//   if (error) return console.error(error);
+//   stop();
+// });
